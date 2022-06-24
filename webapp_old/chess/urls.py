65ca405b3bd1ca8ff674c008284ebe942632ee9a
Manuel Martín <manu.2ka8@gmail.com>
2022-06-24 11:42:07 +0000

    removed webapp_old directory

--- webapp_old/chess/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-app_name = 'chess'
-urlpatterns = [
-	url(r'^$', views.index, name='index'),
-]
--- webapp_old/static/css/backup/README.txt
+++
@@ -1,2 +0,0 @@
-These files were moved here when the new theme was installed, they were conflicting
-with the theme
